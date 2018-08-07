4b270c347ae1644dc2bb2405f0abbca2368c4d58
hindlemail <hindlemail@localhost>
2018-08-07 02:52:59 -0600

    https://trello.com/c/4gCGOtSN/44-geckoanchorelement-needs-finishing

--- Geckofx-Core/DOM/Html/HTMLAnchorElement.cs
+++ Geckofx-Core/DOM/Html/HTMLAnchorElement.cs
@@ -2,16 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Gecko.WebIDL;
 
 namespace Gecko.DOM
 {
     public class GeckoAnchorElement : GeckoHtmlElement
     {
+        private readonly nsISupports _window;
         private /* nsIDOMHTMLAnchorElement */ nsIDOMElement DOMHTMLElement;
+        private Lazy<HTMLAnchorElement> _anchorElement;
+        private Lazy<HTMLLinkElement> _linkElement;
 
         internal GeckoAnchorElement(nsISupports window, /* nsIDOMHTMLAnchorElement */ nsIDOMElement element) : base(window, element)
         {
+            _window = window;
             this.DOMHTMLElement = element;
+            _anchorElement = new Lazy<HTMLAnchorElement>(() => new HTMLAnchorElement((mozIDOMWindowProxy)_window, (nsISupports)element));
+            _linkElement = new Lazy<HTMLLinkElement>(() => new HTMLLinkElement((mozIDOMWindowProxy)_window, (nsISupports)element));
         }
 
         public GeckoAnchorElement(nsISupports window, object element) : base(window, element as /* nsIDOMHTMLElement */nsIDOMElement)
@@ -21,62 +28,62 @@ namespace Gecko.DOM
 
         public string Charset
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetCharsetAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetCharsetAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Charset; }
+            set { _anchorElement.Value.Charset = value; }
         }
 
         public string Coords
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetCoordsAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetCoordsAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Coords; }
+            set { _anchorElement.Value.Coords = value; }
         }
 
         public string Href
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetHrefAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetHrefAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _linkElement.Value.Href; }
+            set { _linkElement.Value.Href = value; }
         }
 
         public string Hreflang
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetHreflangAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetHreflangAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _linkElement.Value.Hreflang; }
+            set { _linkElement.Value.Hreflang = value; }
         }
 
         public string Name
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetNameAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetNameAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Name; }
+            set { _anchorElement.Value.Name = value; }
         }
 
         public string Rel
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetRelAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetRelAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Rel; }
+            set { _anchorElement.Value.Rel = value; }
         }
 
         public string Rev
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetRevAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetRevAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Rev; }
+            set { _anchorElement.Value.Rev = value; }
         }
 
         public string Shape
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetShapeAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetShapeAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Shape; }
+            set { _anchorElement.Value.Shape = value; }
         }
 
         public string Target
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetTargetAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetTargetAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Target; }
+            set { _anchorElement.Value.Target = value; }
         }
 
         public string Type
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetTypeAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetTypeAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _anchorElement.Value.Type; }
+            set { _anchorElement.Value.Type = value; }
         }
     }
 }
