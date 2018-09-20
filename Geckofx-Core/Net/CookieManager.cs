6b44f4ce07b32db2cfd9add2aded88ea2d8d0ae8
hindlemail <tom_hindle@sil.org>
2018-09-20 10:44:51 -0600

    https://bitbucket.org/geckofx/geckofx-60.0/issues/2/cookies-not-working-on-geckofx-60

--- Geckofx-Core/Net/CookieManager.cs
+++ Geckofx-Core/Net/CookieManager.cs
@@ -6,76 +6,73 @@ namespace Gecko
 {
     public static class CookieManager
     {
-        private static ComPtr</* nsICookieManager2 */ nsISupports> _cookieManager;
+        private static ComPtr<nsICookieManager> _cookieManager;
 
         static CookieManager()
         {
-            _cookieManager = Xpcom.GetService2</* nsICookieManager2 */ nsISupports>(Contracts.CookieManager);
+            _cookieManager = Xpcom.GetService2<nsICookieManager>(Contracts.CookieManager);
         }
 
         public static void Add(string host, string path, string name, string value, bool isSecure, bool isHttpOnly,
             bool isSession, long expiry)
         {
-            //_cookieManager.Instance.Add(
-            //    new nsAUTF8String(host),
-            //    new nsAUTF8String(path),
-            //    new nsACString(name),
-            //    new nsACString(value),
-            //    isSecure,
-            //    isHttpOnly,
-            //    isSession,
-            //    expiry);
-            throw new NotImplementedException();
+            var unused = JsVal.FromDouble(0);
+            _cookieManager.Instance.Add(
+                new nsAUTF8String(host),
+                new nsAUTF8String(path),
+                new nsACString(name),
+                new nsACString(value),
+                isSecure,
+                isHttpOnly,
+                isSession,
+                expiry, ref unused, 0, IntPtr.Zero, 0);
         }
 
         public static bool CookieExists(Cookie cookie)
         {
-            //return _cookieManager.Instance.CookieExists(cookie._cookie);
-            throw new NotImplementedException();
+            throw new NotImplementedException("CookieExists requires specifying origin");
+            var unused = JsVal.FromDouble(0);
+            return _cookieManager.Instance.CookieExists(cookie._cookie, unused, IntPtr.Zero, 0);
         }
 
         public static int CountCookiesFromHost(string host)
         {
             // int is big for cookie count :)
-            //return (int) _cookieManager.Instance.CountCookiesFromHost(new nsAUTF8String(host));
-            throw new NotImplementedException();
+            return (int)_cookieManager.Instance.CountCookiesFromHost(new nsAUTF8String(host));            
         }
 
         public static IEnumerator<Cookie> GetCookiesFromHost(string host)
         {
-            //return
-            //    new Collections.GeckoEnumerator<Cookie, nsICookie2>(
-            //        _cookieManager.Instance.GetCookiesFromHost(new nsAUTF8String(host)),
-            //        x => new Cookie(x));
-            throw new NotImplementedException();
+            var unused = JsVal.FromDouble(0);
+            return
+                new Collections.GeckoEnumerator<Cookie, nsICookie2>(
+                    _cookieManager.Instance.GetCookiesFromHost(new nsAUTF8String(host), unused, IntPtr.Zero, 0),
+                    x => x != null ? new Cookie(x) : null);
         }
 
         public static IEnumerator<Cookie> GetEnumerator()
         {
-            //return new Collections.GeckoEnumerator<Cookie, nsICookie2>(_cookieManager.Instance.GetEnumeratorAttribute(),
-            //    x => new Cookie(x));
-            throw new NotImplementedException();
+            return new Collections.GeckoEnumerator<Cookie, nsICookie2>(_cookieManager.Instance.GetEnumeratorAttribute(),
+                x => new Cookie(x));            
         }
 
         public static void ImportCookies(string filename)
         {
-            //_cookieManager.Instance.ImportCookies((nsIFile) Xpcom.NewNativeLocalFile(filename));
-            throw new NotImplementedException();
+            _cookieManager.Instance.ImportCookies((nsIFile)Xpcom.NewNativeLocalFile(filename));            
         }
 
         public static void Remove(string host, string name, string path, bool blocked)
         {
-            //_cookieManager.Instance.Remove(new nsAUTF8String(host),
-            //    new nsACString(name),
-            //    new nsAUTF8String(path),
-            //    blocked);
-            throw new NotImplementedException();
+            var unused = JsVal.FromDouble(0);
+            _cookieManager.Instance.Remove(new nsAUTF8String(host),
+                new nsACString(name),
+                new nsAUTF8String(path),
+                blocked, ref unused, IntPtr.Zero, 4);
         }
 
         public static void RemoveAll()
         {
-            //_cookieManager.Instance.RemoveAll();
-            throw new NotImplementedException();
+            _cookieManager.Instance.RemoveAll();            
         }
     }
 
@@ -89,64 +86,28 @@ namespace Gecko
             _cookie = cookie;
         }
 
-        public long CreationTime
-        {
-            get { return _cookie.GetCreationTimeAttribute(); }
-        }
+        public long CreationTime => _cookie.GetCreationTimeAttribute();
 
-        public long Expiry
-        {
-            get { return _cookie.GetExpiryAttribute(); }
-        }
+        public long Expiry => _cookie.GetExpiryAttribute();
 
-        public string Host
-        {
-            get { return nsString.Get(_cookie.GetHostAttribute); }
-        }
+        public string Host => nsString.Get(_cookie.GetHostAttribute);
 
-        public bool IsDomain
-        {
-            get { return _cookie.GetIsDomainAttribute(); }
-        }
+        public bool IsDomain => _cookie.GetIsDomainAttribute();
 
-        public bool IsHttpOnly
-        {
-            get { return _cookie.GetIsHttpOnlyAttribute(); }
-        }
+        public bool IsHttpOnly => _cookie.GetIsHttpOnlyAttribute();
 
-        public bool IsSecure
-        {
-            get { return _cookie.GetIsSecureAttribute(); }
-        }
+        public bool IsSecure => _cookie.GetIsSecureAttribute();
 
-        public bool IsSession
-        {
-            get { return _cookie.GetIsSessionAttribute(); }
-        }
+        public bool IsSession => _cookie.GetIsSessionAttribute();
 
-        public long LastAccessed
-        {
-            get { return _cookie.GetLastAccessedAttribute(); }
-        }
+        public long LastAccessed => _cookie.GetLastAccessedAttribute();
 
-        public string Name
-        {
-            get { return nsString.Get(_cookie.GetNameAttribute); }
-        }
+        public string Name => nsString.Get(_cookie.GetNameAttribute);
 
-        public string Path
-        {
-            get { return nsString.Get(_cookie.GetPathAttribute); }
-        }
+        public string Path => nsString.Get(_cookie.GetPathAttribute);
 
-        public string RawHost
-        {
-            get { return nsString.Get(_cookie.GetRawHostAttribute); }
-        }
+        public string RawHost => nsString.Get(_cookie.GetRawHostAttribute);
 
-        public string Value
-        {
-            get { return nsString.Get(_cookie.GetValueAttribute); }
-        }
+        public string Value => nsString.Get(_cookie.GetValueAttribute);
     }
 }
---
+++ GeckofxUnitTests/Net/CookieManagerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gecko;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GeckofxUnitTests.Net
+{
+    [TestFixture]
+    class CookieManagerTests
+    {
+        [Test]
+        public void Add_DoesNotCrash()
+        {
+            CookieManager.Add("a", "b", "c", "d", true, true, true, long.MaxValue);
+        }
+
+        [Test]
+        public void Remove_DoesNotCrash()
+        {
+            CookieManager.Remove("a", "b", "c", false);
+        }
+
+        [Test]
+        public void RemoveAll_DoesNotCrash()
+        {
+            CookieManager.RemoveAll();
+        }
+
+        [Test]
+        public void GetCookiesFromHost_NoCookiesOnHost_ReturnsEmptyCollection()
+        {
+            var results = CookieManager.GetCookiesFromHost("somehostwithnocookies");
+            Assert.IsFalse(results.MoveNext());
+            Assert.IsNull(results.Current);
+        }
+
+        [Test]
+        public void GetCookiesFromHost_HostWithSingleCookie_ReturnsSingleCookie()
+        {
+            CookieManager.Add("somehost", "b", "c", "d", true, true, true, long.MaxValue);
+            var results = CookieManager.GetCookiesFromHost("somehost");
+            Assert.IsTrue(results.MoveNext());
+            Assert.AreEqual("d", results.Current.Value);
+            Assert.IsFalse(results.MoveNext());
+        }
+
+        [Test]
+        public void CountCookiesFromHost_ReutnrsNumberOfCookiesOnHost()
+        {
+            CookieManager.Add("myhost", "b", "c", "d", true, true, true, long.MaxValue);
+            Assert.AreEqual(1, CookieManager.CountCookiesFromHost("myhost"));
+        }
+
+        [Test]
+        public void CookieExists_Exists_ThrowsNotImplementedException()
+        {
+            CookieManager.Add("myhost2", "b", "c", "d", true, true, true, long.MaxValue);
+            var results = CookieManager.GetCookiesFromHost("myhost2");
+            Assert.IsTrue(results.MoveNext());
+            var cookie = results.Current;
+            Assert.Throws<NotImplementedException>(() => CookieManager.CookieExists(cookie));
+        }
+    }
+}
