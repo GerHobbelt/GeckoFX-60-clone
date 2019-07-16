f336eac24ea25dfd20b4731819a2de837f7d79c2
hindlemail <hindlemail@localhost>
2019-07-15 22:21:31 -0600

    Implement GeckoScriptElement for Geckofx60.

--- Geckofx-Core/DOM/Html/HTMLScriptElement.cs
+++ Geckofx-Core/DOM/Html/HTMLScriptElement.cs
@@ -2,58 +2,61 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Gecko.WebIDL;
 
 namespace Gecko.DOM
 {
     public class GeckoScriptElement : GeckoHtmlElement
     {
         private /* nsIDOMHTMLScriptElement */ nsIDOMElement DOMHTMLElement;
+        private Lazy<HTMLScriptElement> _scriptElement;
 
         internal GeckoScriptElement(mozIDOMWindowProxy window, /* nsIDOMHTMLScriptElement */ nsIDOMElement element) : base(window, element)
         {
             this.DOMHTMLElement = element;
+            _scriptElement = new Lazy<HTMLScriptElement>(() => new HTMLScriptElement(Window, (nsISupports)element));
         }
 
         public string Text
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetTextAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetTextAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Text; }
+            set { _scriptElement.Value.Text = value; }
         }
 
         public string HtmlFor
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetHtmlForAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetHtmlForAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.HtmlFor; }
+            set { _scriptElement.Value.HtmlFor = value; }
         }
 
         public string Event
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetEventAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetEventAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Event; }
+            set { _scriptElement.Value.Event = value; }
         }
 
         public string Charset
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetCharsetAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetCharsetAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Charset; }
+            set { _scriptElement.Value.Charset = value; }
         }
 
         public bool Defer
         {
-            get { /*return DOMHTMLElement.GetDeferAttribute();*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetDeferAttribute(value);*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Defer; }
+            set { _scriptElement.Value.Defer = value; }
         }
 
         public string Src
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetSrcAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetSrcAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Src; }
+            set { _scriptElement.Value.Src = value; }
         }
 
         public string Type
         {
-            get { /*return nsString.Get(DOMHTMLElement.GetTypeAttribute);*/throw new NotImplementedException(); }
-            set { /*DOMHTMLElement.SetTypeAttribute(new nsAString(value));*/throw new NotImplementedException(); }
+            get { return _scriptElement.Value.Type; }
+            set { _scriptElement.Value.Type = value; }
         }
     }
 }
---
+++ GeckofxUnitTests/dom/GeckoScriptElementTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gecko.DOM;
+using NUnit.Framework;
+
+namespace GeckofxUnitTests.dom
+{
+    [TestFixture()]
+    class GeckoScriptElementTests : BaseElementWrapperTests
+    {
+        [Test]
+        public void Src_SrcSpecified_ReturnsSpecifiedSrc()
+        {
+            _browser.TestLoadHtml("<script src='file:///unknownfile.js'>let a = 2 + 3; console.log(a);</script>");
+            var objectUnderTest = _browser.Document.Body.FirstChild as GeckoScriptElement;
+            Assert.NotNull(objectUnderTest);
+
+            Assert.AreEqual("file:///unknownfile.js", objectUnderTest.Src);
+        }
+
+        [Test]
+        public void Type_StandardType_ReturnsType()
+        {
+            _browser.TestLoadHtml("<script type='text/javascript'>let a = 2 + 3; console.log(a);</script>");
+            var objectUnderTest = _browser.Document.Body.FirstChild as GeckoScriptElement;
+            Assert.NotNull(objectUnderTest);
+
+            Assert.AreEqual("text/javascript", objectUnderTest.Type);
+        }
+
+        [Test]
+        public void Text_InlineScript_ReturnsScriptContents()
+        {
+            var testScript = "let a = 2 + 3; console.log(a);";
+            _browser.TestLoadHtml($"<script>{testScript}</script>");
+            var objectUnderTest = _browser.Document.Body.FirstChild as GeckoScriptElement;
+            Assert.NotNull(objectUnderTest);
+
+            Assert.AreEqual(testScript, objectUnderTest.Text);
+        }
+    }
+}
